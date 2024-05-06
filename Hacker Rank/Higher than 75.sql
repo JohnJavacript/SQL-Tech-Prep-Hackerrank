34a72e789e10dcdfe4646cb9f5c414013617904c
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-05-06 16:25:33 +0300

    final

--- Hacker Rank/Higher than 75.sql
+++ Hacker Rank/Higher than 75.sql
@@ -55,3 +55,11 @@ FROM(
   ORDER BY NAME
 ) Temp
 GROUP BY RowNumber;
+
+-- Binary Tree Nodes
+SELECT N, CASE WHEN P IS NULL THEN 'Root' 
+WHEN(SELECT COUNT(*) FROM BST WHERE P = A.N) > 0 THEN 'Inner'
+ELSE 'Leaf'
+END
+FROM BST A
+ORDER BY N;
