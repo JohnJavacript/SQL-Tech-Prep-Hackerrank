ff1f03f6d40951c951eeb268aaaf27f49fabd5b7
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-05-06 12:32:15 +0300

    final

--- Hacker Rank/Higher than 75.sql
+++ Hacker Rank/Higher than 75.sql
@@ -3,3 +3,13 @@
  SELECT Name FROM STUDENTS
  WHERE Marks >75
  ORDER BY SUBSTR(Name,-3),ID
+
+--Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+
+Input Format
+
+The Employee table containing employee data for a company is described as follows
+ 
+SELECT  name
+FROM Employee
+ORDER BY name;
