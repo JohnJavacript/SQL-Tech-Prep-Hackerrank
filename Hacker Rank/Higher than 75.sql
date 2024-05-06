33deb685bf93b711d0a8d5c4f2a7c47fc2666b21
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2024-05-06 12:45:48 +0300

    final

--- Hacker Rank/Higher than 75.sql
+++ Hacker Rank/Higher than 75.sql
@@ -13,3 +13,8 @@ The Employee table containing employee data for a company is described as follow
 SELECT  name
 FROM Employee
 ORDER BY name;
+
+--Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than  per month who have been employees for less than  months. Sort your result by ascending employee_id.
+SELECT name
+FROM Employee
+WHERE salary >2000 and months <10;
