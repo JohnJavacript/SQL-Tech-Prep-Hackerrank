622b5446ff3be05054dfc17f5522d8c9f5718570
JohnKimaiyo <kimaiyojohn6@gmail.com>
2024-10-05 12:20:12 +0300

    final

--- Hacker Rank/project 1.sql
+++ Hacker Rank/project 1.sql
@@ -33,3 +33,7 @@ FROM CITY;
 --Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, but did not realize her keyboard's  key was broken until after completing the calculation. She wants your help finding the difference between her miscalculation (using salaries with any zeros removed), and the actual average salary.
 
 WSELECT CEIL (AVG (SALARY) - AVG(REPLACE(SALARY, 0, ''))) FROM EMPLOYEES
+
+-- We define an employee's total earnings to be their monthly  worked, and the maximum total earnings to be the maximum total earnings for any employee in the Employee table. Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. Then print these values as  space-separated integers.
+
+SELECT months*salary as earnings, COUNT(*) FROM Employee GROUP BY earnings ORDER BY earnings DESC LIMIT 1;
