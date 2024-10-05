30bfc303f0789a7446698139280b03d6527ddd29
JohnKimaiyo <kimaiyojohn6@gmail.com>
2024-10-05 07:48:52 +0300

    final

--- Hacker Rank/project 1.sql
+++ Hacker Rank/project 1.sql
@@ -29,9 +29,7 @@ SELECT  MAX(POPULATION) - MIN(POPULATION) AS Population_Difference
 FROM CITY;
 
 
---
-SQL
--- Assuming the EMPLOYEES table has the same structure as the provided image:
+
 --Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, but did not realize her keyboard's  key was broken until after completing the calculation. She wants your help finding the difference between her miscalculation (using salaries with any zeros removed), and the actual average salary.
 
 Write a query calculating the amount of error (i.e.:  average monthly salaries), and round it up to the next integer.
